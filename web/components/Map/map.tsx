36d9a7961150ed6c410556e85134ecbdaa83cc46
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-10-14 23:34:42 +0100

    created building map, updated map

--- web/components/Map/map.tsx
+++ web/components/Map/map.tsx
@@ -1,20 +1,26 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { FC, ReactNode } from "react";
 
-const Map = () => {
+interface mapProps {
+  children?: ReactNode;
+}
+
+const Map: FC<mapProps> = ({ children }) => {
   return (
     <MapContainer
       center={[55.872925, -4.288128]}
       zoom={15}
       scrollWheelZoom={false}
-      style={{ height: 400, width: "100%" }}
+      style={{ height: "90vh", width: "100%", zIndex: 1 }}
       zoomControl={false}
-      dragging={false}
+      // dragging={false}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {children}
     </MapContainer>
   );
 };
