e052426b6d8f0eda809736465624be0637fc50d2
Rory McDermid <2566302M@student.gla.ac.uk>
2022-10-14 20:49:49 +0100

    Read in people_data

--- processing/processStatement.py
+++ processing/processStatement.py
@@ -1,7 +1,8 @@
 import csv
 import linecache
 from PersonRaw import PersonRaw
-
+from ast import literal_eval
+import json
 
 
 def main():
@@ -14,13 +15,16 @@ def main():
                 line_count += 1
 
             else:
-                person = PersonRaw(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], [])
+                if row[8] == "N/A":
+                    person = PersonRaw(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], [], [])
+                else:
+                    person = PersonRaw(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], literal_eval(row[8]), [])
+
                 people.append(person)
                 line_count += 1
 
-
-            
-    print(people[3].name)
+    return people
+        
 
 
 if __name__ == "__main__":
