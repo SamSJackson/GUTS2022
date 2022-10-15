774bdf3e49eb7ae04d6da8cd8a5ec6c2c6662358
Sharon Padippurackal <2558994P@student.gla.ac.uk>
2022-10-15 12:07:50 +0100

    fixed times

--- data_visualization/timetabler.py
+++ data_visualization/timetabler.py
@@ -4,10 +4,10 @@ def main():
     # format: location : [(opening,closing),(subject,(lecture time))]
     # if empty string then can be used by anyone
     locationTimes = {"boyd orr building": [(800, 2100), ("computing science", (1000, 1200))],
-                     "james watt building": [(), ("engineering", (1000, 1300))],
-                     "adam smith building": [(), ("Business", (1000, 1100))],
-                     "main building": [(), ("mathematics", (1100, 1300))],
-                     "wolfson medical building": [(), ("medicine", (1300, 1500))],
+                     "james watt building": [(900,1700), ("engineering", (1000, 1300))],
+                     "adam smith building": [(900,1700), ("Business", (1000, 1100))],
+                     "main building": [(900,1700), ("mathematics", (1100, 1300))],
+                     "wolfson medical building": [(900,1700), ("medicine", (1300, 1500))],
                      "glasgow university union": [(900, 2300), ("", (900, 2300))],
                      "the hive": [(2200, 200), ("", (2200, 200))],
                      "guu": [(900, 2300), ("", (900, 2300))],
@@ -20,6 +20,9 @@ def main():
                      "joseph black building": [(900, 1700), ("chemistry", (1000, 1100))],
                      "kelvin building": [(900, 1700), ("physics", (900, 1100))]
                      }
+
+
+
 
     pass
 
